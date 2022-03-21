97ace8d7b62bf7934bdc47095b8c94bc8a895ce4
wonjongseo <visionwill@naver.com>
2022-03-21 20:50:17 +0900

    change column:

--- src/main/java/com/filot/filotshop/product/entity/Image.java
+++ src/main/java/com/filot/filotshop/product/entity/Image.java
@@ -3,6 +3,7 @@ package com.filot.filotshop.product.entity;
 import com.filot.filotshop.commons.entity.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 
@@ -11,6 +12,7 @@ import javax.persistence.*;
 public class Image extends BaseEntity {
     @Id
     @GeneratedValue
+    @Column(name =  "image_id")
     private Long id;
 
     private String url;
